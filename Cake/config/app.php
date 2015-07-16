6af4a9842420a8a0c4715c8cdbda15ce8dcbc5f3
leonardo_guto <leolleo.comp@gmail.com>
2015-07-16 17:55:28 -0300

    ajuste git

--- Cake/config/app.php
+++ Cake/config/app.php
@@ -9,7 +9,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => true,
+    'debug' => True,
 
     /**
      * Configure basic information about the application.
